4ddd55b7ac7ac11ad6e713b82f8aceb872b7b633
shashaneRanasinghe <shashane.ranasinghe@gmail.com>
2023-05-04 13:05:53 +0530

    add test cases

--- internal/delivery/http/handlers/student/studentHandler_test.go
+++ internal/delivery/http/handlers/student/studentHandler_test.go
@@ -1,6 +1,7 @@
 package student
 
 import (
+	"errors"
 	"github.com/golang/mock/gomock"
 	"github.com/gorilla/mux"
 	"github.com/shashaneRanasinghe/simpleAPI/internal/models"
@@ -29,9 +30,11 @@ var (
 		LastName:  "Sainz",
 		Year:      1,
 	}
+	errStudent  = &models.Student{}
 	studentList = []models.Student{student1, student2}
+	ErrResponse = errors.New("error Getting Students")
 
-	//expectedResponseError = `{"status":"Error","data":{"id":0,"firstname":"","lastname":"","year":0},"message":"Error Getting Students "}`
+	expectedResponseError = `{"status":"Error","data":{"id":0,"firstname":"","lastname":"","year":0},"message":"Error Getting Students "}`
 )
 
 func NewMockStudentHandler_HappyPath(ctrl *gomock.Controller) *StudentHandler {
@@ -125,6 +128,111 @@ func TestStudentRoutes_HappyPath(t *testing.T) {
 			requestBody:    `{"searchString":"charl","sortBy": {"column":"firstname","direction":"ASC"},"pagination": {"page":0,"pageSize":2}}`,
 			expectedStatus: 200,
 			expectedBody:   `{"status":"Success","data":{"totalElements":2,"data":[{"id":1,"firstname":"Charles","lastname":"Leclerc","year":3},{"id":2,"firstname":"Carlos","lastname":"Sainz","year":1}]},"message":"Student Queried Successfully"}`,
+		},
+	}
+
+	for _, test := range testCases {
+		req := httptest.NewRequest(test.method, test.url, strings.NewReader(test.requestBody))
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		if w.Code != test.expectedStatus {
+			t.Errorf("Test %s : Expected status code %d, but got %d", test.name, test.expectedStatus, w.Code)
+		}
+
+		if w.Body.String() != test.expectedBody {
+			t.Errorf("Test %s : Expected response body %s, but got %s", test.name, test.expectedBody, w.Body.String())
+		}
+	}
+}
+
+func NewMockStudentHandler_ErrorPath(ctrl *gomock.Controller) *StudentHandler {
+	student := mocks.NewMockStudentUsecase(ctrl)
+
+	student.EXPECT().GetAllStudents().Return(nil, ErrResponse)
+	student.EXPECT().GetStudent(1).Return(errStudent, ErrResponse)
+	student.EXPECT().CreateStudent(&student0).Return(errStudent, ErrResponse)
+	student.EXPECT().UpdateStudent(&student1).Return(errStudent, ErrResponse)
+	student.EXPECT().DeleteStudent(1).Return(errStudent, ErrResponse)
+	student.EXPECT().SearchStudent("charl", models.Pagination{Page: 0, PageSize: 2},
+		models.SortBy{Column: "firstname", Direction: "ASC"}).Return(nil, ErrResponse)
+
+	return &StudentHandler{
+		student: student,
+	}
+}
+
+func TestStudentRoutes_ErrorPath(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	r := mux.NewRouter()
+
+	studentHandler := NewMockStudentHandler_ErrorPath(ctrl)
+
+	r.HandleFunc("/", studentHandler.getAllStudents).Methods("GET")
+	r.HandleFunc("/getStudent/{id}", studentHandler.getStudent).Methods("GET")
+	r.HandleFunc("/", studentHandler.createStudent).Methods("POST")
+	r.HandleFunc("/", studentHandler.updateStudent).Methods("PUT")
+	r.HandleFunc("/{id}", studentHandler.deleteStudent).Methods("DELETE")
+	r.HandleFunc("/search", studentHandler.searchStudents).Methods("GET") //studentHandler := NewStudentHandler(database.NewDatabase().GetConnection())
+
+	testCases := []struct {
+		name           string
+		url            string
+		method         string
+		requestBody    string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "Get All Students",
+			url:            "/",
+			method:         "GET",
+			requestBody:    "",
+			expectedStatus: 500,
+			expectedBody:   `{"status":"Error","data":null,"message":"Error Getting Students "}`,
+		},
+		{
+			name:           "Get Specific Students",
+			url:            "/getStudent/1",
+			method:         "GET",
+			requestBody:    "",
+			expectedStatus: 500,
+			expectedBody:   expectedResponseError,
+		},
+		{
+			name:           "Create Student",
+			url:            "/",
+			method:         "POST",
+			requestBody:    `{"firstname":"Charles","lastname":"Leclerc","year":3}`,
+			expectedStatus: 500,
+			expectedBody:   expectedResponseError,
+		},
+		{
+			name:           "Update Student",
+			url:            "/",
+			method:         "PUT",
+			requestBody:    `{"id":1,"firstname":"Charles","lastname":"Leclerc","year":3}`,
+			expectedStatus: 500,
+			expectedBody:   expectedResponseError,
+		},
+		{
+			name:           "Delete Specific Student",
+			url:            "/1",
+			method:         "DELETE",
+			requestBody:    "",
+			expectedStatus: 500,
+			expectedBody:   `{"status":"Error","data":{"id":0,"firstname":"","lastname":"","year":0},"message":"Error Deleting Student"}`,
+		},
+		{
+			name:           "Search Students",
+			url:            "/search",
+			method:         "GET",
+			requestBody:    `{"searchString":"charl","sortBy": {"column":"firstname","direction":"ASC"},"pagination": {"page":0,"pageSize":2}}`,
+			expectedStatus: 500,
+			expectedBody:   `{"status":"Error","data":{"totalElements":0,"data":null},"message":"Error Getting Students "}`,
 		},
 	}
 
